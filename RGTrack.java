53b446592252b9de9d6f2408b55db3a1e054d8ac
D-Abraham <danielbsalama@gmail.com>
2018-03-16 12:22:25 -0400

    Track:  -drawLane and drawCheckpoint takes color as an arg.  -Track has a dark gray color, lane line has yellow color and checkpoints has black color.  -changed the cars pic names: 0.png to 4.png and 4.png to 0.png.

--- RGTrack.java
+++ RGTrack.java
@@ -95,55 +95,28 @@ public class RGTrack extends JPanel{
       //System.out.println(checkPointList[i]);
     }
   }
-  
-  /**
-   * Written by DA.
-   * Draws a broken line which represent a broken yellow line.
-   * 
-   * @param g 
-   * @param startAtX
-   * @param y
-   * @param gap the empty gap length.
-   */
-  public void drawLane(Graphics g, int startAtX, int y, int gap){
-      for(int x=startAtX+5; x<600; x+=25){
-        g.drawLine(x,y,x+gap,y);   
-    }
-  }
-  /**
-   * Written by DA.
-   * Draws checkpoint (vertical line).
-   * @param g
-   * @param x
-   * @param startAtY
-   * @param gap the empty gap length.
-   */
-  public void drawCheckpiont(Graphics g, int x, int startAtY, int gap){
-      for(int y=startAtY; y<500; y+=100){
-        g.drawLine(x,y,x,y+gap);   
-    }
-  }
-  
-  
+    
   /*
    * creates rectangles to simulate the track
    * creates cars within these rectangles
    */
   public void paintComponent(Graphics g){
     Graphics2D g2 = (Graphics2D) g;
-    g.drawLine(0, 100, 600, 100); //DA:top line (sold line)
-    g.drawLine(0, 500, 600, 500); //DA:botton line (sold line)
-    
-    drawLane(g, 0, 200, 15);
-    drawLane(g, 0, 300, 15);
-    drawLane(g, 0, 400, 15);
+
+    //DA:track's outside lines: 0,100-600,100 and 0,500-600,500.
+    g.setColor(Color.DARK_GRAY); // DA track color
+    g.fillRect(0, 100, 600, 400); 
+    drawLane(g, 0, 200, 15, Color.YELLOW); //DA: broken (dashed) yellow line. 
+    drawLane(g, 0, 300, 15,Color.YELLOW); 
+    drawLane(g, 0, 400, 15,Color.YELLOW); 
     
 
     for(int i = 0; i < vehicleList.length; i++) {
       //Rectangle2D.Double track = new Rectangle2D.Double(0, (i+1)*100, 600, 100);
       //g.drawLine(checkPointList[i]+70, 100, checkPointList[i]+70, 501);
-      drawCheckpiont(g,checkPointList[i]+70, 110, 80);
-      g.drawString("Car " + Integer.toString(i+1),vehicleList[i].getStartAtX()+20 , ((i+1)*130)-(30*i)); // This is for Nic :) <DA>
+      drawCheckpiont(g,checkPointList[i]+70, 110, 80, Color.BLACK); //DA
+      g.setColor(Color.RED);
+      g.drawString("Car " + Integer.toString(i+1),vehicleList[i].getStartAtX()+20 , ((i+1)*130)-(30*i)); // DA: This is for Nic :) 
       
       //g2.draw(track);
       vehicleList[i].paintComponent(g2);
@@ -175,6 +148,39 @@ public class RGTrack extends JPanel{
   
   public RGCar[] getVehicleList() {
     return vehicleList;
+  }
+  
+  /**
+   * Written by DA.
+   * Draws a broken (dashed) line which represent a broken yellow line between
+   * two lanes.
+   * 
+   * @param g 
+   * @param startAtX
+   * @param y
+   * @param gap the empty gap length.
+   * @param color line color.
+   */
+  public void drawLane(Graphics g, int startAtX, int y, int gap, Color color){
+      g.setColor(color);
+      for(int x=startAtX+5; x<600; x+=25){
+        g.drawLine(x,y,x+gap,y);   
+    }
+  }
+  /**
+   * Written by DA.
+   * Draws checkpoint (vertical line).
+   * @param g
+   * @param x
+   * @param startAtY
+   * @param gap the empty gap length.
+   * @param color line color.
+   */
+  public void drawCheckpiont(Graphics g, int x, int startAtY, int gap, Color color){
+      g.setColor(color);
+      for(int y=startAtY; y<500; y+=100){
+        g.drawLine(x,y,x,y+gap);   
+    }
   }
   
   /**
